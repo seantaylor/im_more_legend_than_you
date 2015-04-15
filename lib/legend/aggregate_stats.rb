9a2d395c8bdeb8e9793d297d65edc375a3f11756
Sean Taylor <sean@freerunningtechnologies.com>
2015-04-14 18:16:43 -0700

    Aggregate stats class to grab data from League API response
    They send it broken down by champ and then have a special
    champion with id == 0 for your aggregated stats because they
    hate APIs.
    
    Change-Id: I6490eb77127a73f82e93051d5a303c8160dd0a69

---
+++ lib/legend/aggregate_stats.rb
@@ -0,0 +1,33 @@
+module Legend
+  class AggregateStats
+
+    # When LoL gives us stats they break it down by champion then give us
+    # Champion '0' which is everything aggregated
+    ALL_CHAMPIONS = 0
+
+    def initialize(raw_data:)
+      @raw_data = raw_data
+    end
+
+    def kills
+      all_champion_data['totalChampionKills']
+    end
+
+    def assists
+      all_champion_data['totalAssists']
+    end
+
+    def deaths
+      all_champion_data['totalDeathsPerSession']
+    end
+
+    private
+
+    def all_champion_data
+      raw_data['champions'].select{ |x| x['id'] == ALL_CHAMPIONS }.first['stats']
+    end
+
+    attr_reader :raw_data
+
+  end
+end
