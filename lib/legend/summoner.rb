5736dbc1f564f76db20cd66c11234b5b9643354b
Sean Taylor <sean@freerunningtechnologies.com>
2015-04-14 18:34:20 -0700

    Add methods to get basic stats from the raw data
    Memoize the data and add getters for KDA
    
    Change-Id: I4a70bd24fd66c8e4985ee8f2fcca2179ce2ccd0f

--- lib/legend/summoner.rb
+++ lib/legend/summoner.rb
@@ -10,11 +10,26 @@ module Legend
       @summoner_level = data.level
     end
 
+    def kills
+      stats.kills
+    end
+
+    def deaths
+      stats.deaths
+    end
+
+    def assists
+      stats.assists
+    end
 
     private
 
     def sanitize name
       name.downcase.downcase.gsub(/\s/, "")
+    end
+
+    def stats
+      @stats ||= Legend::Client.new.get_stats(summoner_id: id)
     end
 
   end
