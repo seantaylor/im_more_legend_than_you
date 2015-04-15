cdf4b54c45641ec9af78151bffb62287febecccf
Sean Taylor <sean@freerunningtechnologies.com>
2015-04-14 18:33:41 -0700

    Sanitize the summoner name before trying to get it from API
    API requires that the name doesn't contain any special characters
    or spaces.
    
    Change-Id: I3113af0d598338180bddb815f9ed50664b61c8a5

--- lib/legend/summoner.rb
+++ lib/legend/summoner.rb
@@ -3,11 +3,18 @@ module Legend
     attr_reader :name, :id, :summoner_level
 
     def initialize(name)
-      data = Legend::Client.new.get_summoner(name: name)
+      data = Legend::Client.new.get_summoner(name: sanitize(name))
 
       @name = data.name
       @id = data.id
       @summoner_level = data.level
+    end
+
+
+    private
+
+    def sanitize name
+      name.downcase.downcase.gsub(/\s/, "")
     end
 
   end
